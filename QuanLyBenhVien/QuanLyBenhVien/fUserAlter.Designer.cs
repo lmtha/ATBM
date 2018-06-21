f3bcfa1e323c368f9684333f5092a85e8579a04e
thienperfect <17perfection03@gmail.com>
2018-06-21 11:06:57 +0700

    update 2

--- QuanLyBenhVien/QuanLyBenhVien/fUserAlter.Designer.cs
+++ QuanLyBenhVien/QuanLyBenhVien/fUserAlter.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.panel5 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cbxUserAlterRoleMoi = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbxUserAlterRoleCu = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.btnUserAlterOK = new System.Windows.Forms.Button();
             this.panel6 = new System.Windows.Forms.Panel();
             this.cbxUserAlter = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.cbxUserAlterRoleCu = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.cbxUserAlterRoleMoi = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
@@ -53,10 +53,20 @@
             this.panel8 = new System.Windows.Forms.Panel();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.panel5.SuspendLayout();
-            this.panel6.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -64,6 +74,9 @@
             this.panel4.SuspendLayout();
             this.panel7.SuspendLayout();
             this.panel8.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel11.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel5
@@ -76,6 +89,60 @@
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(260, 180);
             this.panel5.TabIndex = 12;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.cbxUserAlterRoleMoi);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(37, 87);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(193, 36);
+            this.panel2.TabIndex = 11;
+            // 
+            // cbxUserAlterRoleMoi
+            // 
+            this.cbxUserAlterRoleMoi.FormattingEnabled = true;
+            this.cbxUserAlterRoleMoi.Location = new System.Drawing.Point(70, 8);
+            this.cbxUserAlterRoleMoi.Name = "cbxUserAlterRoleMoi";
+            this.cbxUserAlterRoleMoi.Size = new System.Drawing.Size(113, 21);
+            this.cbxUserAlterRoleMoi.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Role mới";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbxUserAlterRoleCu);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(37, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(193, 36);
+            this.panel1.TabIndex = 10;
+            // 
+            // cbxUserAlterRoleCu
+            // 
+            this.cbxUserAlterRoleCu.FormattingEnabled = true;
+            this.cbxUserAlterRoleCu.Location = new System.Drawing.Point(70, 8);
+            this.cbxUserAlterRoleCu.Name = "cbxUserAlterRoleCu";
+            this.cbxUserAlterRoleCu.Size = new System.Drawing.Size(113, 21);
+            this.cbxUserAlterRoleCu.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Role cũ";
             // 
             // btnUserAlterOK
             // 
@@ -115,60 +182,6 @@
             this.label4.TabIndex = 4;
             this.label4.Text = "Tên user";
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.cbxUserAlterRoleCu);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(37, 45);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(193, 36);
-            this.panel1.TabIndex = 10;
-            // 
-            // cbxUserAlterRoleCu
-            // 
-            this.cbxUserAlterRoleCu.FormattingEnabled = true;
-            this.cbxUserAlterRoleCu.Location = new System.Drawing.Point(70, 8);
-            this.cbxUserAlterRoleCu.Name = "cbxUserAlterRoleCu";
-            this.cbxUserAlterRoleCu.Size = new System.Drawing.Size(113, 21);
-            this.cbxUserAlterRoleCu.TabIndex = 9;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Role cũ";
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.cbxUserAlterRoleMoi);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(37, 87);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(193, 36);
-            this.panel2.TabIndex = 11;
-            // 
-            // cbxUserAlterRoleMoi
-            // 
-            this.cbxUserAlterRoleMoi.FormattingEnabled = true;
-            this.cbxUserAlterRoleMoi.Location = new System.Drawing.Point(70, 8);
-            this.cbxUserAlterRoleMoi.Name = "cbxUserAlterRoleMoi";
-            this.cbxUserAlterRoleMoi.Size = new System.Drawing.Size(113, 21);
-            this.cbxUserAlterRoleMoi.TabIndex = 9;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 10);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 17);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Role mới";
-            // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
@@ -176,7 +189,7 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(508, 291);
+            this.tabControl1.Size = new System.Drawing.Size(637, 371);
             this.tabControl1.TabIndex = 13;
             // 
             // tabPage1
@@ -197,19 +210,23 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(500, 265);
+            this.tabPage2.Size = new System.Drawing.Size(629, 345);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Hiệu chỉnh quyền";
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.panel9);
+            this.panel3.Controls.Add(this.panel10);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.panel11);
             this.panel3.Controls.Add(this.panel4);
             this.panel3.Controls.Add(this.panel7);
             this.panel3.Controls.Add(this.button1);
             this.panel3.Controls.Add(this.panel8);
             this.panel3.Location = new System.Drawing.Point(3, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(491, 220);
+            this.panel3.Size = new System.Drawing.Size(605, 309);
             this.panel3.TabIndex = 13;
             // 
             // panel4
@@ -302,12 +319,102 @@
             this.label6.TabIndex = 4;
             this.label6.Text = "Tên user";
             // 
+            // panel9
+            // 
+            this.panel9.Controls.Add(this.comboBox4);
+            this.panel9.Controls.Add(this.label7);
+            this.panel9.Location = new System.Drawing.Point(328, 103);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(254, 41);
+            this.panel9.TabIndex = 20;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(127, 9);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(113, 21);
+            this.comboBox4.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(24, 10);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 17);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Cột thuộc tính";
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.comboBox5);
+            this.panel10.Controls.Add(this.label8);
+            this.panel10.Location = new System.Drawing.Point(302, 45);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(280, 52);
+            this.panel10.TabIndex = 19;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(153, 16);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(113, 21);
+            this.comboBox5.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 10);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(143, 34);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Quyền select, update\r\n             insert, delete";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(455, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Chấp nhận";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel11
+            // 
+            this.panel11.Controls.Add(this.comboBox6);
+            this.panel11.Controls.Add(this.label9);
+            this.panel11.Location = new System.Drawing.Point(359, 3);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(223, 36);
+            this.panel11.TabIndex = 18;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(96, 9);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(113, 21);
+            this.comboBox6.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(23, 10);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 17);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Tên user";
+            // 
             // fUserAlter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(532, 315);
+            this.ClientSize = new System.Drawing.Size(678, 424);
             this.Controls.Add(this.tabControl1);
             this.MaximizeBox = false;
             this.Name = "fUserAlter";
@@ -315,12 +422,12 @@
             this.Text = "Hiệu chỉnh user/role";
             this.Load += new System.EventHandler(this.fUserAlter_Load);
             this.panel5.ResumeLayout(false);
-            this.panel6.ResumeLayout(false);
-            this.panel6.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
@@ -331,6 +438,12 @@
             this.panel7.PerformLayout();
             this.panel8.ResumeLayout(false);
             this.panel8.PerformLayout();
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
+            this.panel11.ResumeLayout(false);
+            this.panel11.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -362,5 +475,15 @@
         private System.Windows.Forms.Panel panel8;
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label9;
     }
 }
